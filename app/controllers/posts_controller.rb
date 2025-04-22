c6626ed0341a406508c353992f31311909f9ec11
Mark Glenn M. Gabuya <gabuya.markglenn@gmail.com>
2025-04-22 18:21:35 +0800

    Added another layer of user validation but this time from the posts controller not just from the views.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -56,25 +56,31 @@ class PostsController < ApplicationController
 
   # PATCH/PUT /posts/1 or /posts/1.json
   def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: "Post was successfully updated." }
-        format.json { render :show, status: :ok, location: @post }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
+    # Adding another layer of security
+    if @post.user_id == current_user.id
+      respond_to do |format|
+        if @post.update(post_params)
+          format.html { redirect_to @post, notice: "Post was successfully updated." }
+          format.json { render :show, status: :ok, location: @post }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @post.errors, status: :unprocessable_entity }
+        end
       end
-    end
+    end   
   end
 
   # DELETE /posts/1 or /posts/1.json
   def destroy
-    @post.destroy!
+    # Adding another layer of security through conditionals
+    if @post.user_id == current_user.id 
+      @post.destroy!
 
-    respond_to do |format|
-      format.html { redirect_to posts_path, status: :see_other, notice: "Post was successfully destroyed." }
-      format.json { head :no_content }
-    end
+      respond_to do |format|
+        format.html { redirect_to posts_path, status: :see_other, notice: "Post was successfully destroyed." }
+        format.json { head :no_content }
+      end
+    end 
   end
 
   private
