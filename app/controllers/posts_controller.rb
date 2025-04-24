bfc30e91c4b6c92fce0f9bf7e76b2f5ae06eaa7a
Mark Glenn M. Gabuya <gabuya.markglenn@gmail.com>
2025-04-24 17:12:46 +0800

    Updated the message whether a video was uploaded, updated, and destroyed.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -53,7 +53,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: "Post was successfully created." }
+        format.html { redirect_to @post, notice: "The video was successfully uploaded." }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -68,7 +68,7 @@ class PostsController < ApplicationController
     if @post.user_id == current_user.id
       respond_to do |format|
         if @post.update(post_params)
-          format.html { redirect_to @post, notice: "Post was successfully updated." }
+          format.html { redirect_to @post, notice: "The video was successfully updated." }
           format.json { render :show, status: :ok, location: @post }
         else
           format.html { render :edit, status: :unprocessable_entity }
@@ -85,7 +85,7 @@ class PostsController < ApplicationController
       @post.destroy!
 
       respond_to do |format|
-        format.html { redirect_to posts_path, status: :see_other, notice: "Post was successfully destroyed." }
+        format.html { redirect_to posts_path, status: :see_other, notice: "The video was successfully destroyed." }
         format.json { head :no_content }
       end
     end 
