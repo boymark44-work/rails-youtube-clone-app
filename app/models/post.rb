0398cd9f7c8452b1de514b6ed172c5a626dd2d84
Mark Glenn M. Gabuya <gabuya.markglenn@gmail.com>
2025-04-24 22:09:52 +0800

    Updated the Post model to also possessed many likes from many users.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ApplicationRecord
     has_one_attached :image
     has_one_attached :video 
     belongs_to :user 
+
+    has_many :likes, dependent: :destroy 
+    has_many :liked_users, through: :likes, source: :user
 end
