52f606d44c21d65c7486287f339e5d8248cadd4d
Mark Glenn M. Gabuya <gabuya.markglenn@gmail.com>
2025-04-24 22:18:29 +0800

    Set a resource like for our posts. A singular resource since one like per one person per one post.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
+  # Devise for Users
   devise_for :users
-  resources :posts
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # Resource and routes for Videos
+  resources :posts do 
+    resource :like, only: [:create] # Singular resource because one like per user per post
+  end
+  
+  get 'pages/media' # My videos route
+  get 'posts/:id/download', to: 'posts#download', as: 'download_post' # Download video route
+
+  # The Root Path
+  root "posts#index"
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  root "posts#index"
-
-  get 'posts/:id/download', to: 'posts#download', as: 'download_post'
-  get 'pages/media'
 end
