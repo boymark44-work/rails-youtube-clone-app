d174c741fc53492670642a09230779ef7f7abf5f
Mark Glenn M. Gabuya <gabuya.markglenn@gmail.com>
2025-05-07 11:33:28 +0800

    Adding an Admin User and its interface.
    - Generate a migration file that would update the User schema by adding an additional field called admin with a boolean attribute whose default value is false. In this way, we may be able to determine if a user is an admin or not.
    - Scope Admin access with a before_action. This is to prevent regular users from accessing admin features by creating a base class for all admin controllers and ensures only authenticated admin users can access them.
    - Create a users controller but dedicated for admin with index, edit, update, and destroy action.
    - Add the admin routes using namespaces.
    - Add the admin user table view through the index action. This is to display all the existing users of the application along with several actions that is available to the admin user.
    - Update the current navbar by adding links that is dedicated only for admin to access.
    - Included as well in the commit are the helpers and test file configurations admin.

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,11 @@ Rails.application.routes.draw do
   # Update User Profile Route 
   resource :profile, only: [:edit, :update]
 
+  # Routes for Admin 
+  namespace :admin do 
+    resources :users, only: [:index, :edit, :update, :destroy]
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
