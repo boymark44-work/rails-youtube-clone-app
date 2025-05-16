60f0594f919e472788d72c7891091e15a920f77c
Mark Glenn M. Gabuya <gabuya.markglenn@gmail.com>
2025-05-16 17:45:06 +0800

    Adding the CRUD Blog functionality to our app.
    - Generate a migration file for the Blog model with title, content, and reference the author id then migrate.
    - Then for the routes, nest the restful resources for blogs to include the CRUD routing.
    - Create a BlogsController under the Author’s Controller to create the CRUD actions of our Blog.
    - Create a view that would render all the Blogs created by the Author.
    - Create a view that would render form for blog creation.
    - Create a view that would render the same form from the blog creation but this time for the edit and update of the blog.
    - Temporarily disable the pages that would render all the blogs publicly meaning all kinds of users can see.

--- app/controllers/authors/blogs_controller.rb
+++ app/controllers/authors/blogs_controller.rb
@@ -1,5 +1,6 @@
 class Authors::BlogsController < ApplicationController
     before_action :authenticate_author!
+    before_action :set_blog, only: [:edit, :update, :destroy]
 
     def index 
         @blogs = current_author.blogs.order(created_at: :desc)
@@ -12,13 +13,34 @@ class Authors::BlogsController < ApplicationController
     def create
         @blog = current_author.blogs.new(blog_params)
         if @blog.save 
-            redirect_to authors_blogs_path, notice: "Blog created."
+            redirect_to authors_blogs_path, notice: "Blog created successfully."
         else 
             render :new 
         end
     end
 
+    def edit
+    end
+
+    def update
+        if @blog.update(blog_params)
+            redirect_to authors_blogs_path, notice: "Blog updated successfully."
+        else 
+            render :edit     
+        end
+    end
+
+    def destroy  
+        @blog.destroy
+        redirect_to authors_blogs_path, notice: "Blog deleted successfully."
+    end
+
     private  
+
+    def set_blog
+        @blog = current_author.blogs.find(params[:id])
+    end
+
 
     def blog_params
         params.require(:blog).permit(:title, :content)
